47393d51ba1e87d4ed12e7fe3dcf80a7501fcf1d
Avi Leavitt <avileavitt@gmail.com>
2025-01-23 09:14:40 -0500

    ensure we run request on correct thread

--- ios/Plugin/InAppReview.swift
+++ ios/Plugin/InAppReview.swift
@@ -4,13 +4,15 @@ import StoreKit
 
 @objc public class InAppReview: NSObject {
     @objc func requestReview(_ call: CAPPluginCall) {
-        if #available(iOS 14.0, *) {
-             if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
-                 SKStoreReviewController.requestReview(in: windowScene)
-             }
-         } else {
-             SKStoreReviewController.requestReview()
-         }
-        call.resolve()
+        DispatchQueue.main.async {
+            if #available(iOS 14.0, *) {
+                if let windowScene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                    SKStoreReviewController.requestReview(in: windowScene)
+                }
+            } else {
+                SKStoreReviewController.requestReview()
+            }
+            call.resolve()
+        }
     }
 }
